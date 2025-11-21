6dee5f52bbb8da1748c916035dfdab057cf110e3
diwangs <27280516+diwangs@users.noreply.github.com>
2025-11-21 13:11:38 -0500

    Changed shebang on test

--- transparent/test.sh
+++ transparent/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S NIXPKGS_ALLOW_UNFREE=1 nix develop --impure --command bash
 
 # Short tests only just to do sanity check
 PACKAGES="querygen utils template-mapping"
